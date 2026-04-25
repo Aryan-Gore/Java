164576b50a9b72a0589dc60124ca1cfdfa6cb980
Aryan Gore <aryangore130@gmail.com>
2026-04-25 14:51:11 +0530

    fraud

--- Fraud_detection.java
+++ Fraud_detection.java
@@ -1,38 +1,38 @@
-/*
-if payment is greater than 50000 and location mismatch 
-and more than 3 transaction in a min
-*/
+    /*
+    if payment is greater than 50000 and location mismatch 
+    and if  more than 3 transaction in a min
+    */
 
-import java.util.*;
+    import java.util.*;
 
-public class Fraud_detection {
+    public class Fraud_detection {
 
-    public static void main(String[] args) {
-        
-        Scanner sc = new Scanner(System.in);
+        public static void main(String[] args) {
+            
+            Scanner sc = new Scanner(System.in);
 
-        System.out.println("Enter amount : ");
-        int amount = sc.nextInt();
+            System.out.println("Enter amount : ");
+            int amount = sc.nextInt();
 
-        System.out.println("location mismatch (1 for yes and 0 for no): ");
-        int location = sc.nextInt();
+            System.out.println("location mismatch (1 for yes and 0 for no): ");
+            int location = sc.nextInt();
 
-        System.out.println("no. of transactions in a  miniute : ");
-        int transactions = sc.nextInt();
+            System.out.println("no. of transactions in a  miniute : ");
+            int transactions = sc.nextInt();
 
-        if(amount > 50000 && location == 1){
-            System.out.println("fraud detected");
+            if(amount > 50000 && location == 1){
+                System.out.println("fraud detected");
+            }
+            else if (transactions > 3)
+                System.out.println("fraud detected");
+            else 
+                System.out.println("fraud not detected");
+
+            sc.close();
+
         }
-        else if (transactions > 3)
-            System.out.println("fraud detected");
-        else 
-            System.out.println("fraud not detected");
 
-        sc.close();
-
+        
     }
-
-    
-}
 
 
