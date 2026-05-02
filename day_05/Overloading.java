4af58e73ef72bf2b63cd80558f8a4a18901ae20f
Aryan Gore <aryangore130@gmail.com>
2026-05-02 15:37:16 +0530

    string

---
+++ day_05/Overloading.java
@@ -0,0 +1,16 @@
+public class Overloading {
+
+    static int add(int a ,int b){
+            return a+b;
+    }
+
+    static int add(int a ,int b,int c){
+            return a+b;
+    }
+
+    public static void main(String[] args) {
+        
+
+    }
+    
+}
