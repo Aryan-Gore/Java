9d3cd43d6b9652037fc3408359c91d571e28f2a1
Aryan Gore <aryangore130@gmail.com>
2026-06-05 17:51:50 +0530

    replace spaces

---
+++ #String/Remove_all_spaces.java
@@ -0,0 +1,10 @@
+public class Remove_all_spaces {
+
+    public static void main(String[] args) {
+        
+        String s  = "Aryan is a good boy";
+
+         s = s.replace(" ", "");
+    }
+    
+}
