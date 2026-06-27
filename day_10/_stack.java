e693970f4cfb3da11e272793bb99fc358fde8901
Aryan Gore <aryangore130@gmail.com>
2026-06-27 14:03:32 +0530

    mid of stack

--- day_10/_stack.java
+++ day_10/_stack.java
@@ -1,3 +1,6 @@
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Stack;
 //Stack implementation Using Linkedlist
 public class _stack {
      
@@ -94,6 +97,108 @@ static void reverseStack(){
  
 }
 
+
+    // Add two stacks
+    static Node add(Node s1, Node s2) {
+
+          Node result = null;
+ 
+        while (s1 != null || s2 != null) {
+
+            int x = 0;
+            int y = 0;
+            int sum = 0;
+
+            if (s1 != null) {
+                x = s1.data;
+                s1 = s1.next;
+            }
+
+            if (s2 != null) {
+                y = s2.data;
+                s2 = s2.next;
+            }
+            
+            sum = x + y;
+
+        Node newNode = new Node(x + y);
+        newNode.next = result;
+        result = newNode;
+       
+        }
+
+        return result;
+    }
+
+
+//merge 2 stacks
+
+static Node merge(Node s1,Node s2){
+
+    if(s1 == null) return s2;
+    if(s2 == null) return s1;
+
+    // Stack<Integer> stack = new Stack<>(); depritiated onwards java 8
+    
+    Deque<Integer> st = new LinkedList<>();
+
+    while(s2 != null){
+        st.push(s2.data);
+        s2 = s2.next;
+    }
+
+          while(!st.isEmpty()){
+                 int val = st.pop();
+                 Node newnode = new Node(val);
+
+                 newnode.next = s1;
+                 s1 = newnode;
+          }
+
+          return s1;
+
+}
+
+static boolean contains(int key){
+
+    if(top == null){
+        return false;
+    }
+
+    Node temp = top;
+
+    while(temp!=null){
+        if(temp.data == key)
+            return true;
+
+        temp = temp.next;
+    }
+
+    return false;
+}
+
+//mid of stack
+static int midAverage() {
+
+    if (top == null)
+        return -1;
+
+    Node slow = top;
+    Node fast = top;
+    Node prev = null;
+
+    while (fast != null && fast.next != null) {
+        prev = slow;
+        slow = slow.next;
+        fast = fast.next.next;
+    }
+
+    if (fast == null)
+        return (prev.data + slow.data) / 2;
+
+    return slow.data;
+}
+//contains , merge 2 stacks , add 2 Stack
 public static void main(String[] args) {
      push(10);
      push(20);
