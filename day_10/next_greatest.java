3e086c75610b5548ec4807e70d2b08cc600af01e
Aryan Gore <aryangore130@gmail.com>
2026-06-27 15:53:24 +0530

    stock span

--- day_10/next_greatest.java
+++ day_10/next_greatest.java
@@ -9,52 +9,51 @@ import java.util.*;
 public class next_greatest {
 
     public static void main(String[] args) {
-        
-        
-        int[] arr = {2,1,9,7,5};
+
+        int[] arr = { 2, 1, 9, 7, 5 };
         int[] ans = new int[arr.length];
         int n = arr.length;
-        ans[n-1] = -1;
+        ans[n - 1] = -1;
         Deque<Integer> stack = new ArrayDeque<>();
-        stack.push(arr[n-1]);
-        
-        for(int i = n-2 ; i >= 0 ; i++){
-            
+
+        stack.push(arr[n - 1]);
+
+        for (int i = n - 2; i >= 0; i++) {
+
             int data = arr[i];
 
             Deque<Integer> temp = new ArrayDeque<>();
-            
-            while(!stack.isEmpty()){
 
-                if(stack.peek() > data){
+            while (!stack.isEmpty()) {
+
+                if (stack.peek() > data) {
                     ans[i] = stack.peek();
                     stack.push(data);
 
-                    while(!temp.isEmpty()){
+                    while (!temp.isEmpty()) {
                         stack.push(temp.pop());
                     }
                     break;
+                } else {
+                    temp.push(stack.pop());
                 }
-               else{
-                   temp.push(stack.pop());
-
-                }
-                  if(stack.isEmpty()){
+                if (stack.isEmpty()) {
                     ans[i] = -1;
-                  }
+                }
 
-                while(!temp.isEmpty()){
-                        stack.push(temp.pop());
-                    }
+                while (!temp.isEmpty()) {
+                    stack.push(temp.pop());
+                }
 
+                
             }
 
         }
 
-        for(int i = 0 ; i < n ;i++){
+        for (int i = 0; i < n; i++) {
             System.out.print(ans[i] + " ");
         }
 
     }
-    
+
 }
---
+++ day_10/prev_greater.java
@@ -0,0 +1,8 @@
+// Question : find the next greatest and prev greater element in a array of size n;
+
+// arr = 2 1 5 9 7 10
+// o/p = 5 5 9 10 10 -1
+
+public class prev_greater {
+    
+}
