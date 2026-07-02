9efafa5617dd9776d025b6a34c51fc0f9467575b
Aryan Gore <aryangore130@gmail.com>
2026-07-02 12:40:55 +0530

    sorting

---
+++ day_11/countsort.java
@@ -0,0 +1,13 @@
+package day_11;
+
+public class countsort {
+    public static void main(String[] args) {
+        int[] arr = {10,7, 1 ,5, 2, 10, 9, 7, 3};
+        int n = arr.length;
+        int[] arr2 = new int[n+1];
+
+        for(int i = 0 ; i < n ; i++){
+             
+        }
+    }
+}
