899eaaa030912aef24033b5c7a69f6c12e24aa91
Aryan Gore <aryangore130@gmail.com>
2026-07-02 15:03:51 +0530

    partition

--- day_11/quick.java
+++ day_11/quick.java
@@ -2,7 +2,32 @@ package day_11;
 
 public class quick {
     
+     
+    static void quick_Sort(int[] arr, int left,int right ){
 
+
+        while(left < right){
+            int pindex = partition(arr, left , right);
+            quick_Sort(arr, left, pindex-1);
+            quick_Sort(arr, left, pindex+1);
+        }
+    }
+
+    static int partition(int[] arr, int left, int right){
+
+            int pivot = arr[right];
+            int i = left -1 ;
+
+            for(int j = left ; j < right ; j++){
+                       if(arr[j] <= pivot){
+                        int temp = arr[j];
+                         arr[j] = arr[i+1];
+                         arr[i+1] = temp;
+                       }
+            }
+            
+        return 0;
+    }
     public static void main(String[] args) {
         
     }
